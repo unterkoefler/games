75eaa1a6faedded2df522fbe20b0b9a61a039127
Willy Unterkoefler <w.unterkoefler@gmail.com>
2024-06-08 12:14:15 -0400

    getting set up

--- flake.nix
+++ flake.nix
@@ -1,3 +1,15 @@
 {
-  outputs = _ : {};
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... } :
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.elmPackages.elm ];
+        };
+      });
 }
